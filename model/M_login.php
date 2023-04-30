0c918ff5a15d1d80fc5a7fc780b2eb3bc641351b
Heler Paez <heler2091@gmail.com>
2023-04-29 21:33:35 -0500

    validaciones de formulario y inicio correcto de session

--- model/M_login.php
+++ model/M_login.php
@@ -1,18 +1,23 @@
 <?php
-include('./Conection.php');
+//include('./Conection.php');
 include('../controller/C_login.php');
 
 $db = new Conection;
 
-if(isset($_GET['ingresar']))
-{
-    $inputData = [
-        'correo_usuario' => mysqli_real_escape_string($db->con,$_GET['correo_usuario']),
-        'contrasena' => mysqli_real_escape_string($db->con,$_GET['contrasena']),
-    ];
+// if(isset($_GET['ingresar']))
+// {
+//     $inputData = [
+//         'correo_usuario' => mysqli_real_escape_string($db->con,$_GET['correo_usuario']),
+//         'contrasena' => mysqli_real_escape_string($db->con,$_GET['contrasena']),
+//     ];
 
-    $logUsuario = new Login($inputData, $inputData);
-    /* $heler = $logUsuario-> login(); */
+//     $logUsuario = new Login($inputData, $inputData);
+//     /* $heler = $logUsuario-> login(); */
 
-}
+// }
+
+$valLogin = new Login;
+$usuario = $valLogin-> validarLogin();
+
+
 ?>
