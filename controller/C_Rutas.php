0622eaa3801beb539d1a195090dd64051c85e0d8
yessy <yessy.palaciohe@amigo.edu.co>
2023-05-09 11:32:42 -0500

    rutas_index

--- controller/C_Rutas.php
+++ controller/C_Rutas.php
@@ -1,27 +1,6 @@
-<?php
-    require_once('../model/Conection.php');
+<?
+include('../model/M_Ruta.php');
 
-    class Ruta{
-        public function __construct(){
-            try {
-                $this->con = new mysqli('localhost','root','','db_aerolinea');//this permite acceder a los atributos
-                echo "Conexion exitosa.";
-            } catch (Exception $pe) {
-                echo "Error conexion BD $dbname :" . $pe->getMessage();
-            }
-
-        }
-
-        public function getRuta(){
-            $query = $this->con->query('SELECT * FROM tbl_rutas');
-            $retorno =[];
-            $i = 0;
-            while($fila = $query->fetch_assoc()){ //devuelve el arreglo
-                $retorno[$i] = $fila;
-                $i++;
-            }
-            return $retorno;
-        }
-    }
-
+$con = new Ruta();
+$lasRutas = $con->getRuta();
 ?>
